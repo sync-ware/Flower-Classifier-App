930bb47389e070147a8364cab1b141ce1ce2233c
sync-ware <saahilms@gmail.com>
2022-03-23 15:04:46 +0000

    Changes in layout TF integration

--- app/src/main/java/com/sync/flowerclassifier/MainActivity.java
+++ app/src/main/java/com/sync/flowerclassifier/MainActivity.java
@@ -1,30 +1,69 @@
 package com.sync.flowerclassifier;
 
+import static android.hardware.SensorManager.getOrientation;
+import static java.lang.Math.min;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.renderscript.Type;
+import android.util.Size;
+import android.view.Surface;
 import android.view.View;
 
+import androidx.camera.core.AspectRatio;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.camera.view.PreviewView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.odml.image.MlImage;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.sync.flowerclassifier.ml.Model;
 
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.task.core.BaseOptions;
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions;
+import org.tensorflow.lite.task.vision.classifier.Classifications;
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
+    private ImageCapture imageCapture;
 
     private ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -66,6 +105,87 @@ public class MainActivity extends AppCompatActivity {
                     Manifest.permission.CAMERA);
             activateCamera();
         }
+
+        FloatingActionButton fab = findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                imageCapture.takePicture(ContextCompat.getMainExecutor(MainActivity.this),
+                        new ImageCapture.OnImageCapturedCallback() {
+
+                            @Override
+                            public void onCaptureSuccess(ImageProxy image){
+                                // Initialization
+                                ImageClassifier.ImageClassifierOptions options =
+                                        ImageClassifier.ImageClassifierOptions.builder()
+                                                .setBaseOptions(BaseOptions.builder().useGpu().build())
+                                                .setMaxResults(1)
+                                                .build();
+                                ImageClassifier imageClassifier =
+                                        null;
+                                try {
+                                    imageClassifier = ImageClassifier.createFromFileAndOptions(
+                                            MainActivity.this, "~/src/main/ml/model.tflite", options);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                                @SuppressLint("UnsafeOptInUsageError") Bitmap bitmap = toBitmap(image.getImage());
+
+                                TensorImage inputImage = TensorImage.fromBitmap(bitmap);
+                                int width = bitmap.getWidth();
+                                int height = bitmap.getHeight();
+                                int cropSize = min(width, height);
+                                ImageProcessingOptions imageOptions =
+                                        ImageProcessingOptions.builder()
+
+                                                // Set the ROI to the center of the image.
+                                                .setRoi(
+                                                        new Rect(
+                                                                /*left=*/ (width - cropSize) / 2,
+                                                                /*top=*/ (height - cropSize) / 2,
+                                                                /*right=*/ (width + cropSize) / 2,
+                                                                /*bottom=*/ (height + cropSize) / 2))
+                                                .build();
+
+                                List<Classifications> results = imageClassifier.classify(inputImage,
+                                        imageOptions);
+                                int x = 0;
+                            }
+                            @Override
+                            public void onError(ImageCaptureException error) {
+                                // insert your code here.
+                            }
+                        }
+                );
+
+            }
+        });
+    }
+
+    private Bitmap toBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
     }
 
     private void activateCamera(){
@@ -84,14 +204,24 @@ public class MainActivity extends AppCompatActivity {
 
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview.Builder()
+                .setTargetResolution(new Size(244, 244))
                 .build();
+
 
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
-        PreviewView previewView = findViewById(R.id.previewView);
+        previewView = findViewById(R.id.previewView);
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        imageCapture =
+                new ImageCapture.Builder()
+                        .setTargetRotation(Surface.ROTATION_0)
+                        .build();
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageCapture, preview);
+
+
+
     }
 }
